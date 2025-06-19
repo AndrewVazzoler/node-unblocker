f17b100b83c7ff2bf44483e3446720729fefd250
Andrew Vazzoler <andrewvazzoler15@gmail.com>
2025-06-19 06:20:15 -0300

    initial commit

--- app.js
+++ app.js
@@ -4,21 +4,19 @@ var app = express();
 
 var unblockerConfig = {
   prefix: "/proxy/",
-   requestMiddleware: [
-    (req, ctx, callback) => {
-      // Headers mais realistas, como se fosse um Chrome no Windows
-      req.headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36';
-      req.headers['accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8';
-      req.headers['accept-language'] = 'en-US,en;q=0.9';
-      req.headers['accept-encoding'] = 'gzip, deflate, br';
-      req.headers['upgrade-insecure-requests'] = '1';
-      req.headers['connection'] = 'keep-alive';
-      // Caso o site exija, pode setar um Referer fake:
-      req.headers['referer'] = 'https://www.google.com/';
-
-      callback();
-    }
-  ]
+  requestMiddleware: [
+    async (req, ctx) => {
+      req.headers["user-agent"] =
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36";
+      req.headers["accept"] =
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+      req.headers["accept-language"] = "en-US,en;q=0.9";
+      req.headers["accept-encoding"] = "gzip, deflate, br";
+      req.headers["upgrade-insecure-requests"] = "1";
+      req.headers["connection"] = "keep-alive";
+      req.headers["referer"] = "https://www.google.com/";
+    },
+  ],
 };
 
 var unblocker = new Unblocker(unblockerConfig);
